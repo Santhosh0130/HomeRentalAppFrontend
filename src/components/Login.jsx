176ea961a401de10be887fc92c7f30f15141288d
Santhosh0130 <bigilsangu@gmail.com>
2025-02-23 17:02:32 +0530

    After 9

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = ({refreshAuth1}) => {
             },
             withCredentials: true,
         }).then((response) => {
-            alert("Sign in Successful");
+            document.cookie = `jwt=${token}; path=/; Secure; SameSite=None;`;
             refreshAuth1();
             navigate("/")
         }).catch((err) => {
